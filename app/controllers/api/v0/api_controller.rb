395f37d64baf93c32802ca332c9d2b9166143825
MJ Rossetti <mrossetti@codeforamerica.org>
2016-01-28 20:28:41 -0800

    responding to api requests with static data

--- app/controllers/api/v0/api_controller.rb
+++ app/controllers/api/v0/api_controller.rb
@@ -1,6 +1,12 @@
 class Api::V0::ApiController < ApplicationController
   def top_violations
-    @violations = [1,2,3,4]
+    @violations = [
+      {"violation_name": "FAILURE TO OBEY", "citation_count": 635},
+      {"violation_name": "URINATING IN PUBLIC", "citation_count": 203},
+      {"violation_name": "SAFETY BELT VIOLATION", "citation_count": 107},
+      {"violation_name": "FOLLOWING TOO CLOSELY", "citation_count": 31},
+      {"violation_name": "SPEEDING 19 to 23 MPH OVER", "citation_count": 20}
+    ]
 
     respond_to do |format|
       format.json { render json: @violations }
