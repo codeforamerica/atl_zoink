dc32a3d2a1a449ba2e42ccfd780fb86130e36d32
MJ Rossetti <mrossetti@codeforamerica.org>
2016-02-01 22:20:39 -0800

    deduping atlanta data to satisfy test

--- spec/workers/atlanta_deduplication_process_spec.rb
+++ spec/workers/atlanta_deduplication_process_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe AtlantaDeduplicationProcess do
   describe ".perform" do
-    it "should not contain any duplicate value combinations." do
+    let(:expected_attributes){
+      ["date", "defendant", "description", "endpoint_ids", "guid", "location", "min_object_id", "object_count", "object_ids", "payable", "room", "time", "violation"]
+    }
+
+    before do
+      AtlantaDeduplicationProcess.perform
+    end
+
+    it "should create a new table which does not contain any duplicate value combinations." do
       sql_string = <<-SQL
         SELECT
           dr.guid
@@ -20,7 +28,13 @@ RSpec.describe AtlantaDeduplicationProcess do
         HAVING count(*) > 1
       SQL
       result = ActiveRecord::Base.connection.execute(sql_string)
-      expect(result.count).to eql(0)
+      expect(result.ntuples).to eql(0)
+    end
+
+    it "should create a new table which contains the expected attributes." do
+      sql_string = "SELECT * FROM atlanta_distinct_objects LIMIT 10;"
+      result = ActiveRecord::Base.connection.execute(sql_string)
+      expect(result.fields.sort).to eql(expected_attributes)
     end
   end
 end
